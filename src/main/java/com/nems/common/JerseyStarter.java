739cfa44fbc8349e51f03d0708760260091ec9df
neelap <neela_reddy@outlook.com>
2016-04-13 19:33:03 +0530

    commit

--- src/main/java/com/nems/common/JerseyStarter.java
+++ src/main/java/com/nems/common/JerseyStarter.java
@@ -1,12 +1,18 @@
 package com.nems.common;
 
 import com.englishtown.vertx.hk2.BootstrapBinder;
+import com.englishtown.vertx.hk2.HK2VerticleFactory;
 import io.vertx.core.*;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.spi.VerticleFactory;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -21,6 +27,18 @@ public class JerseyStarter extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) throws Exception {
+        HttpServer httpServer = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        Optional<VerticleFactory> factory = vertx.verticleFactories()
+                .stream()
+                .filter(f -> f instanceof HK2VerticleFactory)
+                .findFirst();
+
+        if (!factory.isPresent()) {
+            throw new IllegalStateException("The HK2Verticle factory is not registered");
+        }
+
         JsonObject config = new JsonObject()
                 .put("hk2_binder", new JsonArray().add(BootstrapBinder.class.getCanonicalName()))
                 .put("jersey", new JsonObject()
@@ -51,6 +69,8 @@ public class JerseyStarter extends AbstractVerticle {
             logger.error("Error deploying verticle: " + t.getMessage());
             t.printStackTrace();
         }
+
+        httpServer.requestHandler(router::accept).listen(9000);
     }
 
     @Override
